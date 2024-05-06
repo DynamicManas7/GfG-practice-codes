2b5480502966be526d9ce2ee3b00abfe419d11bf
Manas Singh <131545755+DynamicManas7@users.noreply.github.com>
2024-05-06 23:28:44 +0530

    Updated Solution - GfG to GitHub

--- Basic/Reverse a String/reverse-a-string.cpp
+++ Basic/Reverse a String/reverse-a-string.cpp
@@ -16,11 +16,9 @@ class Solution
     {
         // Your code goes here
         
-        int start=0 ,end=str.size()-1;
-        while(start<end){
-            swap(str[start],str[end]);
-            start++;
-            end--;
+          int end = str.size() - 1;
+        for (int start = 0; start < end; start++, end--) {
+            swap(str[start], str[end]);
         }
         return str;
     }
