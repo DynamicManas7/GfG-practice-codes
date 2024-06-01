26165200b842e6386041fc7ee9d784e1162e0141
Manas Singh <131545755+DynamicManas7@users.noreply.github.com>
2024-06-01 18:07:42 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Odd Even Problem/odd-even-problem.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+
+class Solution {
+  public:
+    string oddEven(string s) {
+        // code hereclass Solution {
+        int alpha[]={1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0};
+        for(char c:s)alpha[c-97]++;
+        int a=0;
+        for(int i:alpha)if(i>0&&!(i&1))a++;
+        if(a&1)return "ODD";
+        else return "EVEN";
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    scanf("%d ", &t);
+    while (t--) {
+
+        string s;
+        getline(cin, s);
+
+        Solution obj;
+        string res = obj.oddEven(s);
+
+        cout << res << "\n";
+    }
+}
+
+// } Driver Code Ends
